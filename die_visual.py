541583af1a7a3660dacf13075cb2b7a9707f7624
lukwac123 <lukasz-waclawek@wp.pl>
2024-07-06 19:17:49 +0200

    Rzut kością do gry.

---
+++ die_visual.py
@@ -0,0 +1,12 @@
+from die import Die
+
+# Utworzenie kości typu D6.
+die = Die()
+
+# Wykonanie pewnej liczby rzutów i umiezczenie wyników na liście.
+results = []
+for roll_num in range(100):
+    result = die.roll()
+    results.append(result)
+
+print(results)
