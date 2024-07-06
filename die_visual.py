27bc3b42100c8d6d816cc165d56db0cf96a38e9b
lukwac123 <lukasz-waclawek@wp.pl>
2024-07-06 19:28:26 +0200

    Analiza wymików. Zwiększamy liczbę rzutów do 1000. Tworzymy listę, która przechowuje liczbę wystąpień danej wartości.

--- die_visual.py
+++ die_visual.py
@@ -5,8 +5,15 @@ die = Die()
 
 # Wykonanie pewnej liczby rzutów i umiezczenie wyników na liście.
 results = []
-for roll_num in range(100):
+for roll_num in range(1_000):
     result = die.roll()
     results.append(result)
 
-print(results)
+# Analiza wyników.
+frequencies = []
+poss_results = range(1, die.num_sides+1)
+for value in poss_results:
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+print(frequencies)
